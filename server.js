fb32cc430ad9ff5a66b060a73061ea30e8939001
Ryan Wells <rswells2@gmail.com>
2024-02-29 13:45:48 -0700

    added rollbar

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'fc44c6c5e7b044aca8fac08aa3336d2e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.use(express.static(`${__dirname}/Public`)) // middle ware. this servers up the front end. it is serveing up a static file
